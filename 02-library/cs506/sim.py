842528972581406c07415761268c0b123c7dd092
BingquanCai <bqcai@bu.edu>
2022-02-08 13:09:05 -0500

    2022/02/08 lab2

--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,3 +1,6 @@
+from numpy import dot
+from numpy.linalg import norm
+
 def euclidean_dist(x, y):
     res = 0
     for i in range(len(x)):
@@ -5,12 +8,17 @@ def euclidean_dist(x, y):
     return res**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    #raise NotImplementedError()
+    return sum(abs(val1-val2) for val1,val2 in zip(x,y))
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    #raise NotImplementedError()
+    intersection = len(list(set(x).intersection(y)))
+    union = (len(x)+len(y)) - intersection
+
+    return 1 - (float(intersection) / union)
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
-
+    #raise NotImplementedError()
+    return dot(x,y)/(norm(x)*norm(y))
 # Feel free to add more
